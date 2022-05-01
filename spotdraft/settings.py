20d1ee05ee3a0a531c0461bfc99681b2974f5301
Rohit Sahoo <rohitsahoo25@gmail.com>
2022-05-01 15:11:30 +0530

    Add nose test configuration

--- spotdraft/settings.py
+++ spotdraft/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_nose',
     'rest_framework',
 
     'planets',
@@ -126,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=planets',
+]
